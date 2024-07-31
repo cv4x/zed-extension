ee2e80b55f353ea1026917c4159625ec7eba30a6
Claire West <bokthaku@gmail.com>
2024-07-31 15:15:03 -0400

    Initial working version

---
+++ languages/html/highlights.scm
@@ -0,0 +1,18 @@
+(tag_name) @keyword
+(erroneous_end_tag_name) @keyword
+(doctype) @constant
+(attribute_name) @property
+(attribute_name) @label
+(attribute_value) @string
+(comment) @comment
+
+"=" @punctuation.delimiter
+"\"" @string
+
+[
+  "<"
+  ">"
+  "<!"
+  "</"
+  "/>"
+] @punctuation.bracket
